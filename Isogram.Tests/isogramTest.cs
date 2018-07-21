f2c3f392ce3e21538d7b05b9cdc67432f6353f2a
Ritam Majjumdar <majumdar_ritam@yahoo.com>
2018-07-21 12:45:16 +0530

    Created the project and updated isogramTest.cs and isogram.cs

---
+++ Isogram.Tests/isogramTest.cs
@@ -0,0 +1,57 @@
+using System;
+using Xunit;
+using isogram;
+
+namespace isogram.Tests
+{
+    public class isogramTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(isogram.isIsogram(""));
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            Assert.True(isogram.isIsogram("isogram"));
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            Assert.False(isogram.isIsogram("eleven"));
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            Assert.True(isogram.isIsogram("subdermatoglyphic"));
+        }
+
+        [Fact]
+        public void Test5()
+        {
+            Assert.True(isogram.isIsogram("Alphabet"));
+        }
+
+        [Fact]
+        public void Test6()
+        {
+            Assert.True(isogram.isIsogram("thumbscrew-japingly"));
+        }
+
+        [Fact]
+        public void Test7()
+        {
+            Assert.True(isogram.isIsogram("six-year-old"));
+        }
+
+        [Fact]
+        public void Test8()
+        {
+            Assert.True(isogram.isIsogram("Old Monk"));
+        }
+    }
+}
