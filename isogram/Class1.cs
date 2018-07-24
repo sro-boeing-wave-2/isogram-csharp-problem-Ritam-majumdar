9e085779dc59762ed3c6c9ba87ad88c788820e30
Ritam Majjumdar <majumdar_ritam@yahoo.com>
2018-07-24 11:26:07 +0530

    Rebuilt the solution

--- isogram/Class1.cs
+++ isogram/Class1.cs
@@ -2,8 +2,20 @@
 
 namespace Isogram
 {
-    public class Class1
+    public class isogram
     {
+        public static bool isIsogram(string str)
+        {
+            char[] arr = str.ToCharArray();
+            Array.Sort(arr);
+            for (int i = 0; i < str.Length - 1; i++)
+            {
+                if (arr[i] == arr[i + 1] && arr[i] != '-')
+                    return false;
+            }
+            return true;
+        }
+
 
     }
 }
